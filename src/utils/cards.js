7747a939225b0de978e4861c05d29f5bcb0eedc0
Stefano Calo <stefano.cal92@gmail.com>
2023-02-04 17:26:51 +0100

    Add card array in utils

---
+++ src/utils/cards.js
@@ -0,0 +1,8 @@
+const cards = [
+    {id: 1, frontUrl: ('../../media/1.png')},
+    {id: 2, frontUrl: ('../../media/2.png')},
+    {id: 3, frontUrl: ('../../media/3.png')},
+    {id: 4, frontUrl: ('../../media/4.png')},
+    {id: 5, frontUrl: ('../../media/5.png')},
+    {id: 6, frontUrl: ('../../media/6.png')}
+]
